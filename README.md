839810001ed1dd4ff9e558c68ce92c641c4fa4d6
Andre Bach <nonnormalizable@gmail.com>
2013-03-20 14:08:58 -0700

    Warmup exercise.

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ NgMachineLearningPython
 =======================
 
 Working the homework assignments of the machine learning Coursera course by Andrew Ng, in Python.
+
+Course that inspired this:
+https://www.coursera.org/course/ml
---
+++ ex1/warmUpExercise.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+#   A = WARMUPEXERCISE() is an example function that returns the 5x5 identity matrix
+
+# ============= YOUR CODE HERE ==============
+# Instructions: Return the 5x5 identity matrix 
+#               In octave, we return values by defining which variables
+#               represent the return values (at the top of the file)
+#               and then set them accordingly. 
+
+from pprint import pprint
+import numpy as np
+
+# OK, there's no "the" identity matrix in Python, but a numpy array of arrays
+# seems the appropiate thing  in this context.
+A = np.identity(5)
+pprint(A)
+
+
+
+
+
+# ===========================================
+
