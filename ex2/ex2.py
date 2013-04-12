3373456b180f9d7a337524c9dd5b3c92d40c1231
Andre Bach <nonnormalizable@gmail.com>
2013-04-12 13:46:12 -0700

    Ex2 prob 1.2 complete.

--- ex2/ex2.py
+++ ex2/ex2.py
@@ -4,14 +4,15 @@ import numpy as np
 import pandas as pd
 from pandas import Series, DataFrame
 import matplotlib.pyplot as plt
+from scipy.optimize import minimize
 
 ## Machine Learning Online Class - Exercise 2: Logistic Regression
 
-def plotData(X, y):
+def plotData(X, y, theta=None):
     """
     Plots the data points X and y into a new figure.
     With + for the positive examples and o for the negative examples.
-    X is assumed to be a mx2 DataFrame and y a m len Series.
+    X is assumed to be a m x 2 DataFrame and y a m length Series.
     """
 
     f = plt.figure()
@@ -19,11 +20,18 @@ def plotData(X, y):
     plt.scatter(X[y==1][0], X[y==1][1], marker='+', s=30, label='Admitted')
     p.axes.yaxis.label.set_text('Exam 2 score')
     p.axes.xaxis.label.set_text('Exam 1 score')
+
+    if not theta==None:
+        l = plt.Line2D([20, -1/theta[2]*(theta[1]*20+theta[0])], [-1/theta[1]*(theta[2]*20+theta[0]), 20],
+                       color='black', label='Decision boundary')
+        p.axes.add_line(l)
+
     plt.legend(loc='upper right')
+
     f.show()
     
     return
-
+    
 
 def sigmoid(z):
     """
@@ -57,6 +65,14 @@ def costFunction(theta, X, y):
     return cost, grad
 
 
+def predict(theta, X):
+    """
+    Return predictions for a set of test scores.
+    """
+
+    p = sigmoid(np.dot(X_array,theta)) >= 0.5
+    return p
+
 if __name__ == '__main__':
     ## Initialization
     plt.close('all')
@@ -72,9 +88,9 @@ if __name__ == '__main__':
     #  We start the exercise by first plotting the data to understand the 
     #  the problem we are working with.
 
-    print 'Plotting data with + indicating (y = 1) examples and o indicating (y = 0) examples.'
+    print 'Plotting data with + indicating (y = 1) examples and o indicating (y = 0) examples.\n'
 
-    plotData(X, y)
+    figure = plotData(X, y)
 
     ## ============ Part 2: Compute Cost and Gradient ============
 
@@ -94,60 +110,46 @@ if __name__ == '__main__':
 
     print 'Cost at initial theta (zeros): %f' % cost
     print 'Gradient at initial theta (zeros):', grad
+    print ''
 
+    ## ============= Part 3: Optimizing  =============
+    # From scipy.optimize, the minimize function looks to offer similar functionality
+    # to fminunc.
 
-#    ## ============= Part 3: Optimizing using fminunc  =============
-#    #  In this exercise, you will use a built-in function (fminunc) to find the
-#    #  optimal parameters theta.
-#
-#    #  Set options for fminunc
-#    options = optimset('GradObj', 'on', 'MaxIter', 400)
-#
-#    #  Run fminunc to obtain the optimal theta
-#    #  This function will return theta and the cost 
-#    #[theta, cost] = fminunc(@(t)(costFunction(t, X, y)), initial_theta, options)
-#
-#    # Print theta to screen
-#    print 'Cost at theta found by fminunc: %f' % cost
-#    print 'theta:'
-#    print ' %f' % theta
-#
-#    # Plot Boundary
-#    plotDecisionBoundary(theta, X, y)
-#
-#    # Put some labels 
-#    #hold on
-#    # Labels and Legend
-#    xlabel('Exam 1 score')
-#    ylabel('Exam 2 score')
-#
-#    # Specified in plot order
-#    legend('Admitted', 'Not admitted')
-#    #hold off
-#
-#    print '\nProgram paused. Press enter to continue.'
-#    pause
-#
-#    ## ============== Part 4: Predict and Accuracies ==============
-#    #  After learning the parameters, you'll like to use it to predict the outcomes
-#    #  on unseen data. In this part, you will use the logistic regression model
-#    #  to predict the probability that a student with score 45 on exam 1 and 
-#    #  score 85 on exam 2 will be admitted.
-#    #
-#    #  Furthermore, you will compute the training and test set accuracies of 
-#    #  our model.
-#    #
-#    #  Your task is to complete the code in predict.m
-#
-#    #  Predict probability for a student with score 45 on exam 1 
-#    #  and score 85 on exam 2 
-#
-#    #prob = sigmoid([1 45 85] * theta)
-#    print 'For a student with scores 45 and 85, we predict an admission probability of %f\n' % prob
-#
-#    # Compute accuracy on our training set
-#    p = predict(theta, X)
-#
-#    print 'Train Accuracy: %f' % (mean(double(p == y)) * 100)
+    # Nelder-Mead works fine, and doesn't use the gradient.
+    result_Nelder_Mead = minimize(lambda t: costFunction(t, X_array, y_array)[0],
+                                  initial_theta, method='Nelder-Mead')
+    
+    # BFGS seems to get into a region where np.dot(X, theta) is large, so the sigmoid returns
+    # 1.0 exactly, and the gradient goes infinite. Breaks.
+    result_BFGS = minimize(lambda t: costFunction(t, X_array, y_array),
+                           initial_theta, method='BFGS', jac=True)
+    
+    # Newton-CG works well even without the Hessian.
+    # Per %timeit, about 3x faster than Nelder-Mead.
+    result_Newton_CG = minimize(lambda t: costFunction(t, X_array, y_array),
+                                initial_theta, method='Newton-CG', jac=True)
+    
+    # Print theta to screen
+    print 'Cost at theta found by Nelder-Mead: %f' % result_Nelder_Mead['fun']
+    print 'theta:', result_Nelder_Mead['x']
+    print 'Cost at theta found by Newton-CG:   %f' % result_Newton_CG['fun']
+    print 'theta:', result_Newton_CG['x']
+    print ''
+    theta = result_Nelder_Mead['x']
+    
+    # Plot Boundary
+    plotData(X, y, theta)
 
+    ## ============== Part 4: Predict and Accuracies ==============
+    #  Predict probability for a student with score 45 on exam 1 
+    #  and score 85 on exam 2 
+
+    prob = sigmoid(np.dot(np.array([1, 45, 85]), theta))
+    print 'For a student with scores 45 and 85, we predict an admission probability of %.1f%%' % (prob*100)
+
+    # Compute accuracy on our training set
+    p = predict(theta, X_array)
+
+    print 'Train Accuracy: %.1f%%' % ((p == y_array).mean() * 100)
 
